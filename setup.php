b29f5e2cbb5040a07904c07a9ee041ae5875bf6c
Charles Severance <csev@umich.edu>
2015-08-12 22:50:55 -0400

    First event

--- setup.php
+++ setup.php
@@ -2,8 +2,9 @@
 date_default_timezone_set('America/Detroit');
 
 $live = "2015-08-07 00:30:00";
-$live = "2015-08-07 10:00:00";
+$live = "2015-08-07 09:55:00";
 $url = "http://www.dr-chuck.com";
+$url = "https://plus.google.com/events/c7bd91gf20sg5kao88j3obuo0js";
 
 $date1 = new DateTime($live);
 $now = new DateTime();
